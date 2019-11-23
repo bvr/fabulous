ab2549cc9952332421bc272f3ea680cccb500f89
Roman Hubacek <roman.hubacek@centrum.cz>
2019-11-23 17:15:14 +0100

    Script to create index of Eric's articles and resulting md

---
+++ create_index.pl
@@ -0,0 +1,19 @@
+
+# simple script to create markdown list of all articles on ericlippert.com
+
+use utf8::all;
+use Mojo::UserAgent;
+use Data::Dump;
+
+my $ua = Mojo::UserAgent->new(max_redirects => 5);
+
+my $base_url = 'https://ericlippert.com';
+for my $month ($ua->get($base_url)->res->dom->find('#archives-2 a')->each) {
+    print "\n## [", $month->text, "](", $month->{href}, ")\n";
+    for my $article ($ua->get($month->{href})->res->dom->find('article')->each) {
+        my $title = $article->find('.entry-title > a:nth-child(1)')->first;
+        my @tags  = $article->find('span.tag-links > a')->each;
+        # join(', ', map { $_->text } @tags) can be used to obtain texts, but does not seem useful
+        print " - [", $title->text, "](", $title->{href}, ")\n";
+    }
+}
