3369367a8548b7dc2861bcca3b461d0f3a7aade4
Roman Hubacek <roman.hubacek@centrum.cz>
2023-03-04 19:50:56 +0100

    Generate list of articles as checkboxes

--- create_index.pl
+++ create_index.pl
@@ -12,12 +12,13 @@ my $base_url = 'https://ericlippert.com';
 # dd $ua->get($base_url)->res;
 
 for my $month ($ua->get($base_url)->res->dom->find('#archives-2 a')->each) {
+    warn $month->text, "\n";
     print "\n## [", $month->text, "](", $month->{href}, ")\n";
     for my $article ($ua->get($month->{href})->res->dom->find('article')->each) {
         my $title = $article->find('.entry-title > a:nth-child(1)')->first;
         my @tags  = $article->find('span.tag-links > a')->each;
         # join(', ', map { $_->text } @tags) can be used to obtain texts, but does not seem useful
-        print " - [", $title->text, "](", $title->{href}, ")\n";
+        print " - [ ] [", $title->text, "](", $title->{href}, ")\n";
     }
 }
 
